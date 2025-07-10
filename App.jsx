98e1fac206f80a6d5e3e1213f994f67eee455357
Prutha Nawghare <pruthanawghare@gmail.com>
2025-07-10 16:28:25 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Background from "./components/background"
+import Foreground from "./components/foreground"
+
+
+
+const App = () => {
+  return (
+    <div className='relative w-full h-screen bg-zinc-800'>
+     <Background/>
+     <Foreground/>
+     
+     
+    </div>
+  )
+}
+
+export default App
